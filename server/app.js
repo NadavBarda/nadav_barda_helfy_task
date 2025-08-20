c037387c34915ab57d2fec2cc0946f1269008f19
NadavBarda <nadav.barda3010@gmail.com>
2025-08-20 14:55:15 +0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+
+
+export default app
